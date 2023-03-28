d26658a41f6ecc4ae8d8e7c5cd18afc6be4d0935
scwang1994 <code850.wang@gmail.com>
2023-03-28 16:23:57 +0800

    commit test

--- test.sol
+++ test.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.7;
 
 contract Counter {
-    uint public count = 0;
+    // uint public count = 0;
     uint public balances = 0;
     function counter() public payable {
         require(msg.value == 1000000 gwei,"Amount should be equal to 1000000 Gwei");
         balances += 1000000 gwei;
-        count = count+1;
+        // count = count+1;
     }
 }
