0135b07edbedfde2ae9d3a6e65bcb2fd6ed7578c
Chienwang <songchien.wang@gmail.com>
2023-05-18 22:35:35 +0800

    feat: added week10 class-practice

--- week10/Blockchain-Resource/section3/SimpleSwap/contracts/SimpleSwap.sol
+++ week10/Blockchain-Resource/section3/SimpleSwap/contracts/SimpleSwap.sol
@@ -5,5 +5,88 @@ import { ISimpleSwap } from "./interface/ISimpleSwap.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleSwap is ISimpleSwap, ERC20 {
+    address public tokenA;
+    address public tokenB;
+
+    uint112 private reserve0; // uses single storage slot, accessible via getReserves
+    uint112 private reserve1; // uses single storage slot, accessible via getReserves
+
+    constructor() public {
+        // set K
+    }
+
     // Implement core logic here
+    /// @notice Swap tokenIn for tokenOut with amountIn
+    /// @param tokenIn The address of the token to swap from
+    /// @param tokenOut The address of the token to swap to
+    /// @param amountIn The amount of tokenIn to swap
+    /// @return amountOut The amount of tokenOut received
+    function swap(address tokenIn, address tokenOut, uint256 amountIn) external returns (uint256 amountOut) {
+        tokenA = tokenIn;
+        tokenB = tokenOut;
+
+        return amountOut;
+    }
+
+    /// @notice Add liquidity to the pool
+    /// @param amountAIn The amount of tokenA to add
+    /// @param amountBIn The amount of tokenB to add
+    /// @return amountA The actually amount of tokenA added
+    /// @return amountB The actually amount of tokenB added
+    /// @return liquidity The amount of liquidity minted
+    function addLiquidity(
+        uint256 amountAIn,
+        uint256 amountBIn
+    ) external returns (uint256 amountA, uint256 amountB, uint256 liquidity) {
+        if (reserveA == 0 && reserveB == 0) {
+            (amountA, amountB) = (amountAIn, amountBIn);
+        } else {
+            uint amountBOptimal = UniswapV2Library.quote(amountAIn, reserveA, reserveB);
+            if (amountBOptimal <= amountBIn) {
+                require(amountBOptimal >= amountBMin, "UniswapV2Router: INSUFFICIENT_B_AMOUNT");
+                (amountA, amountB) = (amountAIn, amountBOptimal);
+            } else {
+                uint amountAOptimal = UniswapV2Library.quote(amountBIn, reserveB, reserveA);
+                assert(amountAOptimal <= amountAIn);
+                require(amountAOptimal >= amountAMin, "UniswapV2Router: INSUFFICIENT_A_AMOUNT");
+                (amountA, amountB) = (amountAOptimal, amountBIn);
+            }
+        }
+        address pair = UniswapV2Library.pairFor(factory, tokenA, tokenB);
+        TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
+        TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        liquidity = IUniswapV2Pair(pair).mint(to);
+    }
+
+    /// @notice Remove liquidity from the pool
+    /// @param liquidity The amount of liquidity to remove
+    /// @return amountA The amount of tokenA received
+    /// @return amountB The amount of tokenB received
+    function removeLiquidity(uint256 liquidity) external returns (uint256 amountA, uint256 amountB) {
+        address pair = UniswapV2Library.pairFor(factory, tokenA, tokenB);
+        IUniswapV2Pair(pair).transferFrom(msg.sender, pair, liquidity); // send liquidity to pair
+        (uint amount0, uint amount1) = IUniswapV2Pair(pair).burn(to);
+
+        (address token0, ) = UniswapV2Library.sortTokens(tokenA, tokenB);
+        (amountA, amountB) = tokenA == token0 ? (amount0, amount1) : (amount1, amount0);
+    }
+
+    /// @notice Get the reserves of the pool
+    /// @return reserveA The reserve of tokenA
+    /// @return reserveB The reserve of tokenB
+    function getReserves() external view returns (uint256 reserveA, uint256 reserveB) {
+        return (reserve0, reserve1);
+    }
+
+    /// @notice Get the address of tokenA
+    /// @return tokenA The address of tokenA
+    function getTokenA() external view returns (address tokenA) {
+        return tokenA;
+    }
+
+    /// @notice Get the address of tokenB
+    /// @return tokenB The address of tokenB
+    function getTokenB() external view returns (address tokenB) {
+        return tokenB;
+    }
 }
